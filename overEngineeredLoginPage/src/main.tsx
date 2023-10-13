443cab908b82a28e097f238dd06d67ae8b22f960
Kerem Hacisabanoglu <kerem.hacisabanoglu@adcolony.com>
2023-10-13 20:17:13 +0300

    add hashrouter

--- overEngineeredLoginPage/src/main.tsx
+++ overEngineeredLoginPage/src/main.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './store/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
